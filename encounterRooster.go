d16caca2016e3e9e33d85135aa3d5c618d5ed188
Galdoba <galdoba@mail.ru>
2017-01-18 20:25:21 +1000

    Source Choser (done)

--- encounterRooster.go
+++ encounterRooster.go
@@ -20,7 +20,7 @@ func createRooster() {
 	fmt.Println("Start Creating Rooster")
 	masterIconList = makeIconList()
 	newIcon1 := createIcon(2)
-	newIcon2 := createIcon(2)
+	newIcon2 := createIcon(5)
 	newIcon3 := createIcon(3)
 	masterIconList = addIcon(masterIconList, newIcon1)
 	masterIconList = addIcon(masterIconList, newIcon2)
@@ -28,13 +28,15 @@ func createRooster() {
 	fmt.Println("Add Icons")
 	fmt.Println(masterIconList.iconArray)
 	fmt.Println(len(masterIconList.iconArray))
-
-	for masterIconList.iconArray[0].getIconMcm() > 0 {
+	for masterIconList.iconArray[2].getIconMcm() > 0 {
+		fmt.Println(masterIconList.iconArray, "при старте хода")
 		makeCombatOrder()
-		iconSource = pickSourceIcon(order)
+		fmt.Println(order)
+		pickIconSource(order)
 		doMatrixAction(iconSource)
-		bubbleSort(order)
+		fmt.Println(masterIconList.iconArray, "в конце хода")
 	}
+
 }
 
 func makeCombatOrder() []int {
@@ -47,8 +49,8 @@ func makeCombatOrder() []int {
 		//assert(icon.getIconInitiative() > 0, "Initiative Less than 0")
 		//icon.setIconInitiative(icon.rollInitiative())
 		order[i] = icon.getIconInitiative()
-		fmt.Println("Icon #:", i, icon)
-		fmt.Println("IconID #:", icon.getIconID())
+		//		fmt.Println("Icon #:", i, icon)
+		//		fmt.Println("IconID #:", icon.getIconID())
 		masterIconList.iconArray[i] = icon
 
 	}
@@ -56,20 +58,28 @@ func makeCombatOrder() []int {
 	bubbleSort(order)
 	//order[0], order[2] = order[2], order[0]
 	fmt.Println("Order after sorting:", order)
+	if order[0] < 0 {
+		fmt.Println("Highest ini < 0. Rerolling:", order)
+		allRollInitiative()
+		fmt.Println("Order before sorting:", order)
+		bubbleSort(order)
+		fmt.Println("Order after sorting:", order)
+	}
 	return order
 }
 
-func pickSourceIcon([]int) Icon {
+func pickIconSource([]int) Icon {
 	var icon Icon
 	for i := range masterIconList.iconArray {
 		icon = masterIconList.iconArray[i]
 		if icon.getIconInitiative() == order[0] {
 			iconSource = icon
-			fmt.Println("ходит икона", iconSource.getIconID())
+			fmt.Println("ходит икона выбор", iconSource.getIconID())
+			return iconSource
 		}
 		fmt.Println(i, icon.getIconInitiative(), order[0])
 	}
-
+	fmt.Println("не смогли выбрать IconSource")
 	fmt.Println(masterIconList.iconArray)
 	return iconSource
 }
@@ -77,7 +87,20 @@ func pickSourceIcon([]int) Icon {
 func doMatrixAction(sourceIcon Icon) { //должно быть еще название действия и механизмы выбора
 	iconSource.setIconMcm(iconSource.getIconMcm() - 1)
 	iconSource.setIconInitiative(iconSource.getIconInitiative() - 10)
-	masterIconList.iconArray[0] = iconSource
+	//masterIconList.iconArray[0] = iconSource //Почему 0?
+	fmt.Println(iconSource)
+	//fmt.Println("Start IconSource to Array")
+	//fmt.Println("изменения в аррэе", masterIconList.iconArray)
+	for i := range masterIconList.iconArray {
+		if iconSource.getIconID() == masterIconList.iconArray[i].getIconID() {
+			masterIconList.iconArray[i] = iconSource
+			//		fmt.Println("ходит икона лоарп", iconSource.getIconID())
+			//		fmt.Println("изменения в аррэе", masterIconList.iconArray)
+			//		fmt.Println("End IconSource to Array")
+		}
+		//fmt.Println(i, icon.getIconInitiative(), order[0])
+	}
+	//fmt.Println("no IconSource to Array")
 }
 
 func makeIconList() IconList {
@@ -115,4 +138,15 @@ func bubbleSort(order []int) {
 			}
 		}
 	}
+}
+
+func allRollInitiative() []int {
+	for i := range masterIconList.iconArray {
+		var icon Icon
+		icon = masterIconList.iconArray[i]
+		icon.setIconInitiative(icon.rollInitiative())
+		masterIconList.iconArray[i].setIconInitiative(icon.getIconInitiative())
+		order[i] = masterIconList.iconArray[i].getIconInitiative()
+	}
+	return order
 }
--- run.go
+++ run.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	
+
 	/* All Raolls tests:
 	threshold := 20
 	dicePoolSrc  := 11
 	dicePoolTrgt := 9
 	limit := 4
-	opposedTest(dicePool1, dicePool2, limit) 
+	opposedTest(dicePool1, dicePool2, limit)
 	fmt.Println(opposedTest(dicePoolSrc, dicePoolTrgt, limit))
 	fmt.Println("******************************************")
 	fmt.Println(simpleTest(dicePoolSrc, limit, threshold))
@@ -18,9 +18,17 @@ func main() {
 	fmt.Println("******************************************")
 	fmt.Println("******************************************")
 	fmt.Println(extendedTest(dicePoolSrc, limit, threshold))
-*/
-	
+	*/
+
+	//ввод интеджера
+	/*fmt.Print("Enter integer of characters: ")
+	  var input int
+	  _, err := fmt.Scanf("%d", &input)
+	  if err != nil {
+	   fmt.Println("Error in integer input")
+	  }*/
+
 	createRooster()
 	fmt.Println("******************************************")
-	
+
 }
